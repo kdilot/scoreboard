f4ce641cd1ff8f408a0bdcc7fdd1fd50f71a3933
kdilot <kdilot@gmail.com>
2019-06-24 13:24:15 -0700

    Updates

---
+++ precache-manifest.81d33950ffacc5a65b70eda8f57905f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9dfc7aeef991796597452851b814952",
+    "url": "/pos/index.html"
+  },
+  {
+    "revision": "f699c14cf7fc509e22e9",
+    "url": "/pos/static/css/2.554f77b2.chunk.css"
+  },
+  {
+    "revision": "dbabf81f63a81982426f",
+    "url": "/pos/static/css/main.f5152c9b.chunk.css"
+  },
+  {
+    "revision": "f699c14cf7fc509e22e9",
+    "url": "/pos/static/js/2.1ecf8834.chunk.js"
+  },
+  {
+    "revision": "dbabf81f63a81982426f",
+    "url": "/pos/static/js/main.39761b89.chunk.js"
+  },
+  {
+    "revision": "7ea00d7d64c42d5bdc91",
+    "url": "/pos/static/js/runtime~main.9b935838.js"
+  }
+]);
