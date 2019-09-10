f701984979bbfad1006f60822beaac14504cb717
kdilot <kdilot@gmail.com>
2019-09-10 12:45:00 -0700

    component

---
+++ src/component/index.js
@@ -0,0 +1,8 @@
+export { default as Button } from './widgets/button/Button';
+export { default as GameLayout } from './layout/GameLayout';
+export { default as GameLiveText } from './text/GameLiveText';
+export { default as MainLayout } from './layout/MainLayout';
+export { default as PointLayout } from './layout/PointLayout';
+export { default as RoundNameText } from './text/RoundNameText';
+export { default as ScoreBoardTitle } from './text/ScoreBoardTitle';
+export { default as ScoreLayout } from './layout/ScoreLayout';
